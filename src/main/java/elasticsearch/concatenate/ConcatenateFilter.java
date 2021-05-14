89984f37199625097931496d1b39b2501a852b36
Amrit Sarkar <sarkaramrit2@gmail.com>
2021-05-14 19:44:58 +0530

    6.5.0

--- src/main/java/elasticsearch/concatenate/ConcatenateFilter.java
+++ src/main/java/elasticsearch/concatenate/ConcatenateFilter.java
@@ -23,7 +23,7 @@ public final class ConcatenateFilter extends TokenFilter {
 
     public ConcatenateFilter(TokenStream input, String tokenSeparator, int incrementGap) {
         super(input);
-        this.tokenSeparator = tokenSeparator!=null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
+        this.tokenSeparator = tokenSeparator != null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
         this.incrementGap = incrementGap;
     }
 
@@ -32,34 +32,34 @@ public final class ConcatenateFilter extends TokenFilter {
         boolean empty = false;
         builder.setLength(0);
 
-	if(recheckPrevious) {
-	   restoreState(previousState);
-	   // append the term of the current token
-           builder.append(termAtt.buffer(), 0, termAtt.length());
-           recheckPrevious = false;
-	}
+        if (recheckPrevious) {
+            restoreState(previousState);
+            // append the term of the current token
+            builder.append(termAtt.buffer(), 0, termAtt.length());
+            recheckPrevious = false;
+        }
 
         while (input.incrementToken()) {
-	    if(posIncrAtt.getPositionIncrement() <= incrementGap) {
-                if (builder.length()>0) {
+            if (posIncrAtt.getPositionIncrement() <= incrementGap) {
+                if (builder.length() > 0) {
                     // append the token separator
                     builder.append(tokenSeparator);
                 }
                 // append the term of the current token
                 builder.append(termAtt.buffer(), 0, termAtt.length());
             } else {
-		// we have found a new element in the array, the next token should start from this one
+                // we have found a new element in the array, the next token should start from this one
                 recheckPrevious = true;
-		previousState = captureState();
-	        break;
-	    }
-	}
+                previousState = captureState();
+                break;
+            }
+        }
 
-        if (builder.length()>0) {
+        if (builder.length() > 0) {
             termAtt.setEmpty().append(builder);
-	    if(!recheckPrevious) {
-	        empty = true;
-	    }
+            if (!recheckPrevious) {
+                empty = true;
+            }
         }
 
         return empty;
